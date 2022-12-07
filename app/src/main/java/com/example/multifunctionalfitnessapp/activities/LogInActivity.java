fd964f92e90edf05479f6161400aa3d1737cb5a5
Berkan Yıkılmaz <berkan.yikilmaz@ug.bilkent.edu.tr>
2022-12-07 10:13:50 +0300

    add log in and sign up activities

---
+++ app/src/main/java/com/example/multifunctionalfitnessapp/activities/LogInActivity.java
@@ -0,0 +1,16 @@
+package com.example.multifunctionalfitnessapp.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.multifunctionalfitnessapp.R;
+
+public class LogInActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.log_in_screen);
+    }
+}
---
+++ app/src/main/java/com/example/multifunctionalfitnessapp/activities/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.multifunctionalfitnessapp.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.multifunctionalfitnessapp.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    //A small change
+    /* Some Tries For User Profile Page
+        username = (EditText) findViewById(R.id.editTextTextUsername);
+        box1 = (CheckBox) findViewById(R.id.checkBox4);
+        box1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String Username = username.getText().toString();
+                Toast.makeText(MainActivity.this,"Username is: " + username, Toast.LENGTH_SHORT );
+            }
+        });*/
+}
