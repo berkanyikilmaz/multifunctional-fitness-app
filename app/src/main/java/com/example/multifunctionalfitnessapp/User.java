511b3832eb1dfab0534ee488ca29e64c855be033
Emre Dinç <emredinc09@gmail.com>
2022-12-07 14:10:33 +0300

    Constructors for User and NormalUser

--- app/src/main/java/com/example/multifunctionalfitnessapp/User.java
+++ app/src/main/java/com/example/multifunctionalfitnessapp/User.java
@@ -8,8 +8,12 @@ public abstract class User {
     String phoneNumber;
     String email;
 
-    public User() {
-        
+    public User(String name, String surname, String password, String phoneNumber, String email) {
+        this.name = name;
+        this.surname = surname;
+        this.password = password;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
     }
 
     void displayProfile() {}
