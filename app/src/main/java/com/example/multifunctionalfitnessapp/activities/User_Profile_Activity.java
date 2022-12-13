239eb1261c5f3c798946307331dc356391aed0ea
Emre Dinç <emredinc09@gmail.com>
2022-12-13 16:04:41 +0300

    Updated user profile activity class. wrote the method to update fields

--- app/src/main/java/com/example/multifunctionalfitnessapp/activities/User_Profile_Activity.java
+++ app/src/main/java/com/example/multifunctionalfitnessapp/activities/User_Profile_Activity.java
@@ -5,7 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.EditText;
 
+import com.example.multifunctionalfitnessapp.NormalUser;
 import com.example.multifunctionalfitnessapp.R;
+import com.example.multifunctionalfitnessapp.User;
 
 public class User_Profile_Activity extends AppCompatActivity {
 
@@ -15,13 +17,14 @@ public class User_Profile_Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.user_profile_menu);
-        displayContents();
+        User user = new NormalUser("", "", "", "", "", "");
+        displayContents(user);
         // buttons and their onClick methods should be added here
         // buttons will function as ways to making changes to the attributes of the user
         // changing user's attributes will be similar to sign-up
     }
 
-    public void displayContents() {
+    public void displayContents(User user) {
         name = findViewById(R.id.editTextTextUsername);
         surname = findViewById(R.id.editTextTextPersonSurname);
         email = findViewById(R.id.editTextTextEmailAddress);
@@ -29,6 +32,12 @@ public class User_Profile_Activity extends AppCompatActivity {
         password = findViewById(R.id.editTextTextPassword);
         userName = findViewById(R.id.editTextTextPersonName);
         // we should set these according to the user
+        name.setText(user.getName());
+        surname.setText(user.getSurname());
+        email.setText(user.getEmail());
+        phoneNumber.setText(user.getPhoneNumber());
+        password.setText(user.getPassword());
+        userName.setText(user.getUsername());
     }
 
     public void updateContents() {
