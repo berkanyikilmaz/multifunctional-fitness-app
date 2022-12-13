d6973b84d13a3e43b7afabaefe732d84a0143401
Emre Dinç <emredinc09@gmail.com>
2022-12-13 16:21:33 +0300

    Make appointment activity

---
+++ app/src/main/java/com/example/multifunctionalfitnessapp/activities/Make_Appointment_Activity.java
@@ -0,0 +1,16 @@
+package com.example.multifunctionalfitnessapp.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.multifunctionalfitnessapp.R;
+
+public class Make_Appointment_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.make_appointment);
+    }
+}
