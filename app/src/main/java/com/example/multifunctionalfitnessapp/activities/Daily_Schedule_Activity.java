6b2d69ea1f906fad754f7c62e7d92cf1fb199075
Emre Dinç <emredinc09@gmail.com>
2022-12-14 15:26:52 +0300

    Changed button back to TextView

--- app/src/main/java/com/example/multifunctionalfitnessapp/activities/Daily_Schedule_Activity.java
+++ app/src/main/java/com/example/multifunctionalfitnessapp/activities/Daily_Schedule_Activity.java
@@ -35,76 +35,76 @@ public class Daily_Schedule_Activity extends AppCompatActivity {
                 // we should display the appointed facility
                 String facilityName = interval.getAppointedFacility().getName();
                 if (i == 0) {
-                    ((Button) findViewById(R.id.Hour1)).setText("appointment at " + facilityName);
+                    ((TextView) findViewById(R.id.Hour1)).setText("appointment at " + facilityName);
                 }
                 else if (i == 1) {
-                    ((Button) findViewById(R.id.Hour2)).setText("appointment at " + facilityName);
+                    ((TextView) findViewById(R.id.Hour2)).setText("appointment at " + facilityName);
                 }
                 else if (i == 2) {
-                    ((Button) findViewById(R.id.Hour3)).setText("appointment at " + facilityName);
+                    ((TextView) findViewById(R.id.Hour3)).setText("appointment at " + facilityName);
                 }
                 else if (i == 3) {
-                    ((Button) findViewById(R.id.Hour4)).setText("appointment at " + facilityName);
+                    ((TextView) findViewById(R.id.Hour4)).setText("appointment at " + facilityName);
                 }
                 else if (i == 4) {
-                    ((Button) findViewById(R.id.Hour5)).setText("appointment at " + facilityName);
+                    ((TextView) findViewById(R.id.Hour5)).setText("appointment at " + facilityName);
                 }
                 else if (i == 5) {
-                    ((Button) findViewById(R.id.Hour6)).setText("appointment at " + facilityName);
+                    ((TextView) findViewById(R.id.Hour6)).setText("appointment at " + facilityName);
                 }
                 else if (i == 6) {
-                    ((Button) findViewById(R.id.Hour7)).setText("appointment at " + facilityName);
+                    ((TextView) findViewById(R.id.Hour7)).setText("appointment at " + facilityName);
                 }
                 else if (i == 7) {
-                    ((Button) findViewById(R.id.Hour8)).setText("appointment at " + facilityName);
+                    ((TextView) findViewById(R.id.Hour8)).setText("appointment at " + facilityName);
                 }
                 else if (i == 8) {
-                    ((Button) findViewById(R.id.Hour9)).setText("appointment at " + facilityName);
+                    ((TextView) findViewById(R.id.Hour9)).setText("appointment at " + facilityName);
                 }
                 else if (i == 9) {
-                    ((Button) findViewById(R.id.Hour10)).setText("appointment at " + facilityName);
+                    ((TextView) findViewById(R.id.Hour10)).setText("appointment at " + facilityName);
                 }
                 else if (i == 10) {
-                    ((Button) findViewById(R.id.Hour11)).setText("appointment at " + facilityName);
+                    ((TextView) findViewById(R.id.Hour11)).setText("appointment at " + facilityName);
                 }
                 else if (i == 11) {
-                    ((Button) findViewById(R.id.Hour12)).setText("appointment at " + facilityName);
+                    ((TextView) findViewById(R.id.Hour12)).setText("appointment at " + facilityName);
                 }
                 else if (i == 12) {
-                    ((Button) findViewById(R.id.Hour13)).setText("appointment at " + facilityName);
+                    ((TextView) findViewById(R.id.Hour13)).setText("appointment at " + facilityName);
                 }
                 else if (i == 13) {
-                    ((Button) findViewById(R.id.Hour14)).setText("appointment at " + facilityName);
+                    ((TextView) findViewById(R.id.Hour14)).setText("appointment at " + facilityName);
                 }
                 else if (i == 14) {
-                    ((Button) findViewById(R.id.Hour15)).setText("appointment at " + facilityName);
+                    ((TextView) findViewById(R.id.Hour15)).setText("appointment at " + facilityName);
                 }
                 else if (i == 15) {
-                    ((Button) findViewById(R.id.Hour16)).setText("appointment at " + facilityName);
+                    ((TextView) findViewById(R.id.Hour16)).setText("appointment at " + facilityName);
                 }
                 else if (i == 16) {
-                    ((Button) findViewById(R.id.Hour17)).setText("appointment at " + facilityName);
+                    ((TextView) findViewById(R.id.Hour17)).setText("appointment at " + facilityName);
                 }
                 else if (i == 17) {
-                    ((Button) findViewById(R.id.Hour18)).setText("appointment at " + facilityName);
+                    ((TextView) findViewById(R.id.Hour18)).setText("appointment at " + facilityName);
                 }
                 else if (i == 18) {
-                    ((Button) findViewById(R.id.Hour19)).setText("appointment at " + facilityName);
+                    ((TextView) findViewById(R.id.Hour19)).setText("appointment at " + facilityName);
                 }
                 else if (i == 19) {
-                    ((Button) findViewById(R.id.Hour20)).setText("appointment at " + facilityName);
+                    ((TextView) findViewById(R.id.Hour20)).setText("appointment at " + facilityName);
                 }
                 else if (i == 20) {
-                    ((Button) findViewById(R.id.Hour21)).setText("appointment at " + facilityName);
+                    ((TextView) findViewById(R.id.Hour21)).setText("appointment at " + facilityName);
                 }
                 else if (i == 21) {
-                    ((Button) findViewById(R.id.Hour22)).setText("appointment at " + facilityName);
+                    ((TextView) findViewById(R.id.Hour22)).setText("appointment at " + facilityName);
                 }
                 else if (i == 22) {
-                    ((Button) findViewById(R.id.Hour23)).setText("appointment at " + facilityName);
+                    ((TextView) findViewById(R.id.Hour23)).setText("appointment at " + facilityName);
                 }
                 else if (i == 23) {
-                    ((Button) findViewById(R.id.Hour24)).setText("appointment at " + facilityName);
+                    ((TextView) findViewById(R.id.Hour24)).setText("appointment at " + facilityName);
                 }
 
             }
