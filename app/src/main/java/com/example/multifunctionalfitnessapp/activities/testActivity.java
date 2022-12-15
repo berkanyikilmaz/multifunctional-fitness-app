da586e942548fbaa7e6bf7948c63e897f03975ec
mehmet dedeler <mehmet.dedeler@ug.bilkent.edu.tr>
2022-12-15 13:21:14 +0300

    selecting day for weekly schedule view

---
+++ app/src/main/java/com/example/multifunctionalfitnessapp/activities/testActivity.java
@@ -0,0 +1,47 @@
+package com.example.multifunctionalfitnessapp.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Toast;
+
+import com.example.multifunctionalfitnessapp.R;
+
+import java.util.ArrayList;
+
+public class testActivity extends AppCompatActivity {
+    String[] days = new String[]{"monday", "tuesday,thursday"};
+    AutoCompleteTextView autoCompleteTextView2my;
+    ArrayAdapter<String> adapterItems;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test);
+
+        autoCompleteTextView2my = findViewById(R.id.autoCompleteTextView2);
+
+        adapterItems = new ArrayAdapter<String>(this,R.layout.list_item_test,days);
+
+        autoCompleteTextView2my.setAdapter(adapterItems);
+
+        autoCompleteTextView2my.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                String item = adapterView.getItemAtPosition(i).toString();
+                Toast.makeText(getApplicationContext(),"Item: "+item,Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        //autoCompleteDropdown = new findViewById(R.id.autoCompleteTextView2);
+
+
+        //val days = new getResources().getStringArray(R.array.days);
+        //val m = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,days);
+        //autoCompleteTextView.setAdapter(m);
+    }
+}
